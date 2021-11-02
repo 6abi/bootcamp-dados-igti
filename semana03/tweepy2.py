42e6dc2e6145646a211d377c0bca79da53a27e54
Barbara <6abi.cardoso@gmail.com>
2021-11-02 00:08:03 -0300

    Analise de sentimento - leitura e contagem tweets

--- semana03/tweepy2.py
+++ semana03/tweepy2.py
@@ -1,17 +1,51 @@
 #imports
 from tweepy import OAuthHandler
 from tweepy import Stream
-import tweepy
+from tweepy import StreamListener
 import socket
 import json
 from decouple import config
 
 #Set up the credencials
 consumer_key = config("consumer_key")
-comsumer_secret = config("comsumer_secret")
+consumer_secret = config("consumer_secret")
 access_token = config("access_token")
 access_secret = config("access_secret")
 
-class  MyStreamListener(tweepy.StreamListener):
-    def __init__(self):
-        pass
+class TweetsListener(StreamListener):
+    """Standard features"""
+    def __init__(self, csocket):
+        self.client_socket = csocket
+        self.count = 0
+        self.limit = 30
+
+
+    def on_data(self, data):
+        """reads and stores the tweets, checks the amount read and sends the message to the socket"""
+        try:
+            msg = json.loads(data)
+            self.count += 1
+            if self.count <= self.limit:
+                print(msg['text'].encode('utf-8'))
+                self.client_socket.send(msg['text'].encode('utf-8'))
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        """Check for error messages"""
+        print(status)
+        return True
+
+    def sendData(c_socket):
+        """Authentication Twitter API and sending data"""
+
+        #Authentication
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token,access_secret)
+
+        twitter_stream = Stream(auth, TweetsListener(c_socket)) #define the type of conection
+        twitter_stream.filter(track=['Bolsonaro'])
+
+
---
+++ src - Copia/variables.py
@@ -0,0 +1,16 @@
+FILE_PATHS = {
+    'local': {
+        'basics': './data/tp/title_basics.tsv',
+        'ratings': './data/tp/title_ratings.tsv'
+        },
+    # 'cloud':{
+    #     'basics': 'gs://edc_bootamp_data/data/title_basics.tsv',
+    #     'ratings': 'gs://edc_bootamp_data/data/title_ratings.tsv',
+    # }
+}
+
+
+# SAVE_PATH = {
+#     'local': './data/imdb/title_basics_with_rating',
+#     # 'cloud': 'gs://edc_bootamp_data/data/title_basics_with_rating',
+# }
